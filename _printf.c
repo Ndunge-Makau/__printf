24ebf731a49e6ffdde3c012d7b6c2726a189539d
Ndunge-Makau <winniemakau@outlook.com>
2022-07-12 11:51:09 -0700

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -1,33 +1,46 @@
 #include "main.h"
 
 /**
- * _printf - prints a string according to a format
- * @format: THe character string
- * Return: The number of characters printed
+ * _printf - produces output accodring to a format
+ * @format: character string
+ * Return: Number of characters printed
  */
 
 int _printf(const char *format, ...)
 {
-	st_fmt st_format[] = {
-		{"c", func_char},
-		{"s", func_string},
-		{"d", func_digit},
-		{"i", func_digit},
-		{"%", func_percent},
-		{"b", func_binary_convert},
-		{"u", func_unsig_int},
-		{"o", func_octal_convert},
-		{"x", func_hex_Lowcase_convert},
-		{"X", func_hex_Upcase_convert},
-		{"S", func_stringUppercase},
-		{"r", func_revstr},
-		{"R", func_rot13},
-		{NULL, NULL}};
-	va_list list;
-	int count = 0;
+	int (*pfunc)(va_list, flags_t *);
+	const char *p;
+	va_list arguments;
+	flags_t flags = {0, 0, 0};
 
-	va_start(list, format);
-	count =	get_match_func(format, list, st_format);
-	va_end(list);
+	resister int count = o;
+
+	va_start(arguments, format);
+	if (!format || (format[0] == '%' && !format[1]))
+		return (-1);
+	if (format[0] == '%' && format[1] == ' ' && !format[2])
+		return (-1);
+	for (p = format; *p; p++)
+	{
+		if (*p == '%')
+		{
+			p++;
+			if (*p == '%')
+			{
+				count += _putchar('%');
+				continue;
+			}
+			while (get_flag(*p, &flags))
+				p++;
+			pfunct = get_print(*p);
+			count += (pfunct)
+				? pfunc(arguments, &flags)
+				: _printf("%%%c", *p);
+		}
+		else
+			count += _putchar(*p);
+	}
+	_putchar(-1);
+	va_end(arguments);
 	return (count);
 }
